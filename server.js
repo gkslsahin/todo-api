bf15c97c7ff5c0d9605990c3628dd8e30a740616
gkslsahin <gkslshn26@gmail.com>
2018-11-27 06:24:52 +0300

    added database connection

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000
+var PORT = 3000;
+
+/***********Database connection************************************* */
+var db = require("./db");
+
+db.sequelize.sync().then(function(){
+    console.log("Database connection is succesfully");
+});
+
+
 app.listen(PORT,function(){
     console.log("Express listening on "+ PORT+" !");
-})
+});
