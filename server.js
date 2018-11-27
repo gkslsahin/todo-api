0e5e2cb3ae1c1db30c58cc02d34508e2dd8a14d5
gkslsahin <gkslshn26@gmail.com>
2018-11-27 23:12:49 +0300

    added get methods

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ var PORT = 3000;
 /***********Database connection************************************* */
 var db = require("./db");
 
+
+app.get("/todos",function(req,res){
+
+    res.send("All elements listed");
+
+});
+
+
 db.sequelize.sync().then(function(){
     console.log("Database connection is succesfully");
+
+    app.listen(PORT,function(){
+        console.log("Express listening on "+ PORT+" !");
+    });
+
 });
 
 
-app.listen(PORT,function(){
-    console.log("Express listening on "+ PORT+" !");
-});
