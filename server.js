027167f05f4770a96a47d75451b03298010d30e6
gkslsahin <gkslshn26@gmail.com>
2018-11-29 01:01:30 +0300

    updated delete, list and update methods

--- server.js
+++ server.js
@@ -9,26 +9,105 @@ app.use(bodyParser.json());
 
 app.get("/todos",function(req,res){
 
-    res.send("All elements listed");
+    db.Todo.findAll({
+        where : {
+            complated : false
+        }
+    }).then(function(todos){
+
+        res.json(todos);
+    })
 
 });
 
 app.post("/todos",function(req,res){
 
     let body = _.pick(req.body,"description","completed");
-    console.log(body);
-
+    db.Todo.create(body).then(function(todo){
+        res.json(todo.toJSON());
+    },function(e){
+        //res.json(e.toJSON());
+        res.status(500).send();
+    })
 
 });
 
 app.put("/todos/:id",function(req,res){
 
-    res.send("update with put methods");
+    let todoId = req.params.id;
+
+    let body = _.pick(req.body, "description", "completed");
+
+    let attributes = {};
+
+    if(body.hasOwnProperty("description")){
+        attributes.description = body.description;
+    }
+
+    if(body.hasOwnProperty("completed")){
+        attributes.completed = body.completed;
+    }
+
+
+    db.Todo.findOne({
+        where : {
+            id : todoId
+        }
+    }).then(function(todo){
+        if(todo){
+
+            todo.update(attributes).then(function(todo){
+
+                res.json(todo.toJSON());
+
+
+            },function(e){
+                res.status(400).send();
+            })
+
+
+
+        }else{
+            res.status(404).send({
+                error : "aranan id bulunamadı!!!"
+            })
+        }
+    },function(){
+        res.status(500).send();
+    })
+
+
+
 });
 
-app.delete("/todos",function(req,res){
+app.delete("/todos/:id",function(req,res){
 
-    res.send("delete with delete methods");
+    let todoId = req.params.id;
+
+    db.Todo.destroy({
+        where : {
+            id : todoId
+        }
+    }).then(function(rowDeleted){
+
+        if(rowDeleted === 0){
+
+            res.status(404).send({
+                error : "cannot faund entry id"
+            });
+
+        }else{
+
+            res.status(204).send();
+
+        }
+
+
+    },function(){
+        res.status(500).send();
+    })
+
+
 });
 
 db.sequelize.sync().then(function(){
